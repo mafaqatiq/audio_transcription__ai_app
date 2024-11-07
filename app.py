e6d65708f1cc50f360f72080006ca211ccec476e
Afaq <168016382+mafaqatiq@users.noreply.github.com>
2024-11-07 16:17:14 +0500

    update
    do

--- app.py
+++ app.py
@@ -37,4 +37,4 @@ def translate_text():
     return jsonify({"translated_text": translated.text})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
